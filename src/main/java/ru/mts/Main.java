244cae644acd9bfd7bd0fbb3cd15633dfe5c34de
“Rodion <sushchev2002@gmail.com>
2023-12-09 13:30:21 +0300

    ДЗ1
    Чистка
    
    ДЗ1 сквош!

---
+++ src/main/java/ru/mts/Main.java
@@ -0,0 +1,16 @@
+package ru.mts;
+
+import static ru.mts.DiscountCalculator.calculateDiscount;
+
+public class Main {
+    public static void main(String[] args) {
+        DiscountCalculator discountCalculator1 = new DiscountCalculator(1, 100.0, 0.75);
+        calculateDiscount(discountCalculator1);
+
+        DiscountCalculator discountCalculator2 = new DiscountCalculator(1, 100.0, 42.575);
+        calculateDiscount(discountCalculator2);
+
+        DiscountCalculator discountCalculator3 = new DiscountCalculator(1, 100.0, 59.1);
+        calculateDiscount(discountCalculator3);
+    }
+}
